11d0e75d62ccfba86596f951188d500a2e1cc296
Vinicius Fantinatto <vifanti@gmail.com>
2020-06-17 01:15:40 -0300

    new folder

--- src/Pages/Dashboard/index.tsx
+++ src/Pages/Dashboard/index.tsx
@@ -1,5 +1,131 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Dashboard: React.FC = () => <h1>Dashboard</h1>;
+import { FiPower, FiClock } from 'react-icons/fi';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  NextAppointment,
+  Section,
+  Appointment,
+  Calendar,
+} from './styles';
+
+import logoImg from '../../assets/logo.svg';
+import { useAuth } from '../../hooks/auth';
+
+const Dashboard: React.FC = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const { signOut, user } = useAuth();
+
+  return (
+    <Container>
+      <Header>
+        <HeaderContent>
+          <img src={logoImg} alt="GoBarber" />
+
+          <Profile>
+            <img src={user.avatar_url} alt={user.name} />
+
+            <div>
+              <span>Bem-Vindo,</span>
+              <strong>{user.name}</strong>
+            </div>
+          </Profile>
+
+          <button type="button" onClick={signOut}>
+            <FiPower />
+          </button>
+        </HeaderContent>
+      </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-feira</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img
+                src="https://avatars3.githubusercontent.com/u/42790333?s=460&u=7671611f1fbbd3759a5b06e17392d1f7774de525&v=4"
+                alt="Vinicius Fantinatto"
+              />
+
+              <strong>Vinicius Fantinatto</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+
+          <Section>
+            <strong>Manhã</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars3.githubusercontent.com/u/42790333?s=460&u=7671611f1fbbd3759a5b06e17392d1f7774de525&v=4"
+                  alt="Vinicius Fantinatto"
+                />
+
+                <strong>Vinicius Fantinatto</strong>
+              </div>
+            </Appointment>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars3.githubusercontent.com/u/42790333?s=460&u=7671611f1fbbd3759a5b06e17392d1f7774de525&v=4"
+                  alt="Vinicius Fantinatto"
+                />
+
+                <strong>Vinicius Fantinatto</strong>
+              </div>
+            </Appointment>
+          </Section>
+          <Section>
+            <strong>Tarde</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars3.githubusercontent.com/u/42790333?s=460&u=7671611f1fbbd3759a5b06e17392d1f7774de525&v=4"
+                  alt="Vinicius Fantinatto"
+                />
+
+                <strong>Vinicius Fantinatto</strong>
+              </div>
+            </Appointment>
+          </Section>
+        </Schedule>
+        <Calendar />
+      </Content>
+    </Container>
+  );
+};
 
 export default Dashboard;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,12 +5,17 @@ import Route from './Route';
 
 import SignIn from '../Pages/SignIn';
 import SignUp from '../Pages/SignUp';
+import ForgotPassword from '../Pages/ForgotPassword';
+import ResetPassword from '../Pages/ResetPassword';
+
 import Dashboard from '../Pages/Dashboard';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={SignIn} />
     <Route path="/signup" exact component={SignUp} />
+    <Route path="/forgot-password" exact component={ForgotPassword} />
+    <Route path="/reset-password" exact component={ResetPassword} />
 
     <Route path="/dashboard" exact component={Dashboard} isPrivate />
   </Switch>
